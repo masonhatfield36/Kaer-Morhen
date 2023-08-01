2215021cacd714d6c55a2ea4d4b8870c91a90655
Mason <mhatfie1@my.athens.edu>
2023-07-31 23:40:40 -0500

    Creating Starter App

---
+++ components/home/welcome/Welcome.jsx
@@ -0,0 +1,26 @@
+import { useState } from 'react'
+import { 
+  View, 
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList 
+} from 'react-native'
+import { useRouter } from 'expo-router';
+
+import styles from './welcome.style'
+import { icons, SIZES } from '../../../constants';
+
+const Welcome = () => {
+  const router = useRouter();
+
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.userName}>Hello Mason</Text>
+    </View>
+  )
+}
+
+export default Welcome
