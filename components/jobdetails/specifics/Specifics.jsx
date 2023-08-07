5ca5f8ec92012f573b20041f8f144e47d2831653
Mason <mhatfie1@my.athens.edu>
2023-08-07 00:12:47 -0500

    Finished Application

--- components/jobdetails/specifics/Specifics.jsx
+++ components/jobdetails/specifics/Specifics.jsx
@@ -5,8 +5,17 @@ import styles from './specifics.style'
 
 const Specifics = () => {
   return (
-    <View>
-      <Text>Specifics</Text>
+    <View style={styles.container}>
+      <Text style={styles.title}>{title}:</Text>
+
+      <View style={styles.pointsContainer}>
+        {points.map((item, index) => (
+          <View style={styles.pointWrapper} key={item + index}>
+            <View style={styles.pointDot} />
+            <Text style={styles.pointText}>{item}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   )
 }
